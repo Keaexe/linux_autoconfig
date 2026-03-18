d2fc826ab089f271f5553f8c328e27cd9c9444f1
Keaexe <pierreolivierpetit@proton.me>
2026-03-18 10:49:49 +0100

    feat: fastfetch config

--- installAll.sh
+++ installAll.sh
@@ -15,7 +15,8 @@ mv ~/.bashrc ~/.bashrc.bak #bashrc
 ln -s ConfigFile/.bashrc ~/.bashrc
 mv ~/.config/starship.toml ~/.config/starship.toml.bak # catpuccine mocha starship 
 ln -s ConfigFile/starship.toml ~/.config/starship.toml
-
+mv ~/.config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc.bak # catpuccine mocha starship 
+ln -s ConfigFile/fastfetchConfig.jsonc ~/.config/fastfetch/config.jsonc
 
 # Additional packages
 yay -S --noconfirm --needed vesktop tor rust devtoolbox onlyoffice isoimagewriter nextcloud-client beeper-v4-bin vlc kdeconnect gabutdm localsend
