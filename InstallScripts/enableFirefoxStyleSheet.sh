0f8957292e64ad051afb6a199dd61ddbc758dc5a
Keaexe <pierreolivierpetit@proton.me>
2026-03-31 11:34:07 +0200

    fix(firefoxEnableStyle): remove unwanted behavior

--- InstallScripts/enableFirefoxStyleSheet.sh
+++ InstallScripts/enableFirefoxStyleSheet.sh
@@ -4,22 +4,13 @@
 PROFILE_DIR=$(find ~/.mozilla/firefox/ -maxdepth 1 -type d -name "*.default-release" | head -n 1)
 while [ -z "$PROFILE_DIR" ]; do
     echo "Could not find Firefox profile."
-    pacman -Qi firefox
+    pacman -Qi firefox > /dev/null
     if [[ $? != 0 ]]; then
         echo "Firefox has not been installed, script failed"
         exit 1
     fi
-    ans=''
-    while [[ $ans != 'S' && $ans != 's' && $ans != 'c' && $ans != 'C' ]]; do
-        read -p "Would you like re-Scan or Create a new profile ? [S/c]" ans
-    done
-    if [[ $ans != 'c' || $ans != 'C' ]]; then
-        read -p "Firefox profile name" PROFILE_DIR
-        firefox -P $PROFILE_DIR
-        if [[ $? != 0 ]]; then
-            PROFILE_DIR=''
-        fi
-    fi
+    echo "Please create a profile"
+    firefox -P $PROFILE_DIR
 done
 
 echo "Targeting profile: $PROFILE_DIR"
