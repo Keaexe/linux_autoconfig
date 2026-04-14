7a9c18f53ffe96eaef11cd6dccfaee5b3db0f7c5
Keaexe <pierreolivierpetit@proton.me>
2026-04-14 11:00:45 +0200

    feat(zen): auto modifications of about:config

--- InstallScripts/zen.sh
+++ InstallScripts/zen.sh
@@ -3,8 +3,8 @@
 yay -S --noconfirm --needed zen-browser
 xdg-settings set default-web-browser zen-browser.desktop
 zen-browser -CreateProfile "WebApps"
-echo "Zen browser will open the config, please turn on the \"compact on startup\" option then close the window"
-sleep 1
-zen-browser -P WebApps about:config
+PROFILE_DIR=$(grep -E 'Path=.*WebApps' ~/.zen/profiles.ini | cut -d'=' -f2)
+PROFILE_PATH="$HOME/.zen/$PROFILE_DIR"
+echo 'user_pref("zen.view.compact.enable-at-startup", true);' >> "$PROFILE_PATH/prefs.js"
 sed -i "s|setsid.*2}\"|zen-browser -P WebApps --new-window \"\$1\"|g" ~/.local/share/omarchy/bin/omarchy-launch-webapp
 echo "Zen should now be the WebApps handler"
